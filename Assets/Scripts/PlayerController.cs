433d47d0246285873be13d5fc8c7e27801078fcf
KadyshevS <kadyshev74@gmail.com>
2024-10-26 16:08:29 +0500

    Added Temp folder to .gitignore

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,41 @@
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    void Start()
-    {
-        
+    public float sensetivityX;
+    public float sensetivityY;
+    public float moveSpeed;
+    public Camera headCamera;
+    
+    CharacterController characterController;
+    float xRotation;
+    float yRotation;
+    Vector3 velocity;
+
+    void Start() {
+        characterController = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
-    void Update()
-    {
-        Vector3 inputDir = Input.GetAxis()
+    void Update() {
+        float mouseX = Input.GetAxisRaw("Mouse X") * sensetivityX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * sensetivityY;
+
+        yRotation += mouseX;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.rotation = Quaternion.Euler(0f, yRotation, 0f); 
+        headCamera.transform.rotation = Quaternion.Euler(xRotation, yRotation, 0f);
+    }
+
+    void FixedUpdate() {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveZ = Input.GetAxisRaw("Vertical");
+        var moveDir = transform.forward * moveZ + transform.right * moveX;
+        moveDir = moveDir.normalized * moveSpeed * Time.deltaTime;
+        characterController.Move(moveDir);
     }
 }
